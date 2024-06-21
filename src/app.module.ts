b34ae8fefeba95d5fd709102f9ef2a6eb91e3485
taekyun-yoon <tk.yoon0105@gmail.com>
2024-06-21 21:45:29 +0900

    feat: Add global JWT authentication and Public decorator

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import postgresConfig from './config/postgres.config';
 import { VideoModule } from './video/video.module';
+import { AuthModule } from './auth/auth.module';
+import jwtConfig from './config/jwt.config';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [postgresConfig],
+      load: [postgresConfig, jwtConfig],
       envFilePath: `.env.${process.env.NODE_ENV}`,
     }),
     TypeOrmModule.forRootAsync({
@@ -24,11 +26,14 @@ import { VideoModule } from './video/video.module';
           database: configService.get('postgres.database'),
           username: configService.get('postgres.username'),
           password: configService.get('postgres.password'),
-          autoLoadEntities: true,
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          autoLoadEntities: false,
+          synchronize: false
         };
 
         if (configService.get('NODE_ENV') === 'development' || 'test') {
           obj = Object.assign(obj, {
+            //엔티티 변경 사항을 자동으로 데이터베이스 스키마에 반영
             synchronize: true,
             logging: true,
           });
@@ -37,9 +42,16 @@ import { VideoModule } from './video/video.module';
       },
     }),
     UserModule,
-    VideoModule
+    VideoModule,
+    AuthModule
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [
+    {
+      //모든 요청에 대해 전역적으로 JwtAuth 인증 수행
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    }
+  ],
 })
 export class AppModule {}
