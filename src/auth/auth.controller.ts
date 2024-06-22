c3da443f412e8df47fec0affabe9282d8aed1fa7
taekyun-yoon <tk.yoon0105@gmail.com>
2024-06-22 16:36:11 +0900

    feat: apply custom swaager deocrator to auth.controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,10 @@
 import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
-import { ApiExtraModels, ApiTags } from '@nestjs/swagger';
+import { ApiCreatedResponse, ApiExtraModels, ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { LoginReqDto, SignupReqDto } from './dto/req.dto';
 import { Public } from 'src/common/decorator/public.decorator';
 import { LoginResDto, SignupResDto } from './dto/res.dto';
+import { ApiGetResponse, ApiPostResponse } from 'src/common/decorator/swagger.decorator';
 
 @ApiTags('Auth')
 @ApiExtraModels(SignupResDto, LoginResDto)
@@ -12,6 +13,7 @@ export class AuthController {
     constructor(private authService: AuthService) {}
 
     @Public()
+    @ApiPostResponse(SignupResDto)
     @Post('signup')
     async signup(@Body() { email, password, passwordConfirm }: SignupReqDto): Promise<SignupResDto> {
         if(password !== passwordConfirm) throw new BadRequestException('Password and PasswordConfirm is not matched.');
@@ -20,6 +22,7 @@ export class AuthController {
     }
 
     @Public()
+    @ApiGetResponse(LoginResDto)
     @Post('login')
     async login(@Body() { email, password }: LoginReqDto): Promise<LoginResDto> {
         return this.authService.login(email, password);
