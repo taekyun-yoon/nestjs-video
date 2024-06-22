5066ee16d4a0752d9938bec03c780383e53a96ee
taekyun-yoon <tk.yoon0105@gmail.com>
2024-06-22 17:35:42 +0900

    test: add unit test for UserService findOneByEmail method

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,18 +1,37 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from './entity/user.entity';
+
+class MockRepository {
+  async findOneBy(query) {
+    const user: User = new User();
+    user.email = query.email;
+    return user;
+  }
+}
 
 describe('UserService', () => {
-  let service: UserService;
+  let userService: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        //데이터베이스 종속성을 끊으면서 mockRepo로 유저 서비스 단위 테스트 
+        {
+          provide: getRepositoryToken(User),
+          useClass: MockRepository,
+        },
+      ],
     }).compile();
 
-    service = module.get<UserService>(UserService);
+    userService = module.get<UserService>(UserService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should be defined', async() => {
+    const email = 'example@example.com';
+    const result = await userService.findOneByEmail(email);
+    expect(result.email).toBe(email);
   });
 });
