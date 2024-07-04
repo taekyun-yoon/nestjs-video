28b954e246128e9ff00f046cba02f445eff6c0d7
taekyun-yoon <tk.yoon0105@gmail.com>
2024-07-04 21:25:12 +0900

    feat: update logging

--- src/common/middleware/logging.middleware.ts
+++ src/common/middleware/logging.middleware.ts
@@ -1,14 +1,23 @@
-import { Injectable, NestMiddleware } from "@nestjs/common";
+import { Injectable, Logger, NestMiddleware } from "@nestjs/common";
 import { NextFunction, Request, Response } from "express";
 
 @Injectable()
 export class LoggingMiddleware implements NestMiddleware {
+    private logger = new Logger('HTTP');
+
     use(req: Request, res: Response, next: NextFunction) {
+
         const userAgent = req.headers['user-agent'] as string;
         const ipAddress = req.ip || req.headers['x-forwarded-for'];
 
         req['userAgent'] = userAgent;
         req['ipAddress'] = ipAddress;
+
+        res.on('close', () => {
+            const { statusCode } = res;
+            const contentLength = res.get('content-length');
+            this.logger.log(`${req.method} ${req.url} ${req.statusCode} ${contentLength} - ${userAgent} ${ipAddress}`);
+        })
 
         next();
     }
